27dc24a74ca93870810dc584fda0ebc80e7c8159
Flavien Petton <pettonflavien1@gmail.com>
2023-10-17 16:14:49 +0200

    'finit'

---
+++ Calculatrice/calculatrice.js
@@ -0,0 +1,49 @@
+function addition(){
+    var a =document.getElementById("t1").value;
+    var b =document.getElementById("t2").value;
+    var c = Number(a)+Number(b);
+
+    t3.value= c;
+}
+function soustraction(){
+    var a =document.getElementById("t1").value;
+    var b =document.getElementById("t2").value;
+    var c = Number(a)-Number(b);
+
+    t3.value= c;
+}
+function Multiplication(){
+    var a =document.getElementById("t1").value;
+    var b =document.getElementById("t2").value;
+    var c = Number(a)*Number(b);
+
+    t3.value= c;
+}
+function Division(){
+    var a =document.getElementById("t1").value;
+    var b =document.getElementById("t2").value;
+    var c = Number(a)/Number(b);
+
+    t3.value= c;
+}
+function permute(){
+    var a =document.getElementById("t1").value;
+    var b =document.getElementById("t2").value; 
+    t1.value= b;
+    t2.value= a;
+}
+function parite(){
+    var a =document.getElementById("t1").value;
+    var b =document.getElementById("t2").value;
+    var c = Number(a)+Number(b);
+    var d = Number(c)%"2";
+
+    t4.value= d;
+
+    if (d== 1)
+    t4.value = "Nombre IMPAIR";
+    else {
+     t4.value = "NOMBRE PAIR";
+    }
+
+}
