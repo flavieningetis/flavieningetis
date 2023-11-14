e429cac5a6642cf8235dbb2dc3ecb79a0c1adf0c
Flavien Petton <pettonflavien1@gmail.com>
2023-11-14 13:47:53 +0100

    kk

--- tp9/identification.js
+++ tp9/identification.js
@@ -1,12 +1,12 @@
-function login(){
+function seconnecter3(){
     var login = document.getElementById("t1").value;
-    var password = document.getElementById("h1").value;
+    var password = document.getElementById("t2").value;
     if(login == "admin" && password == "admin")
     {
-        window.location.href='yes.html'
+        window.location.href='ok.html'
     } 
     else {
-        window.location.href='non.html'
+        window.location.href='lose.html'
 }
 }
 
